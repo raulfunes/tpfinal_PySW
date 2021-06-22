9c3414f9da09a839d6c5009c31455afc1293c39f
Raul Funes <raulsergiofunes@gmail.com>
2021-06-21 23:30:57 -0300

    Resubida

--- proyecto-backend/routes/usuario.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-//defino controlador para el manejo de CRUD
-const usuarioCtrl = require('./../controllers/usuario.controller');
-// definiendo rutas
-router.post('/', usuarioCtrl.createUsuario);
-router.post('/login', usuarioCtrl.loginUsuario);
-//exportacion del modulo de rutas
-module.exports = router;
