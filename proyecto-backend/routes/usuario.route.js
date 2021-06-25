e52d60a8af678e37426811d40d9170d2415c95a3
Raul Funes <raulsergiofunes@gmail.com>
2021-06-25 13:58:01 -0300

    Usuario Post

--- proyecto-backend/routes/usuario.route.js
+++ proyecto-backend/routes/usuario.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //defino controlador para el manejo de CRUD
 const usuarioCtrl = require('./../controllers/usuario.controller');
 // definiendo rutas
-router.post('/', [authCtrl.verifyToken, authCtrl.isEntrenador], usuarioCtrl.createUsuario);
+router.post('/', usuarioCtrl.createUsuario);
 router.post('/login', usuarioCtrl.loginUsuario);
 router.get('/:persona',[authCtrl.verifyToken, authCtrl.isEntrenador], usuarioCtrl.getUsuario);
 //exportacion del modulo de rutas
